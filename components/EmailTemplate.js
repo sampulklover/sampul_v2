2332bc32a9f9dd63ea820ce9373a65b8f89de723
iklankerjakosongmalaysia <134831327+devOneGithub@users.noreply.github.com>
2024-02-28 22:21:15 +0800

    v1.0.27

--- components/EmailTemplate.js
+++ components/EmailTemplate.js
@@ -4,6 +4,30 @@ export const inviteBelove = ({ name }) => {
   return (
     <div>
       <h1>Salam and Greetings {name},</h1>
+      <p>
+        We are delighted to inform you that you had been selected as a "Primary
+        Co-Sampul/Secondary Co-Sampul" for "customer name". We hope that you
+        have been briefed by "customer name" on the appointment and are ready to
+        take on the responsibilities.
+      </p>
+      <p>
+        Briefly, your responsibilities as a Co-Sampul is to be given the
+        information of "customer name"’s wasiat/will which contain all assets’
+        information and wishes at the point of death and assist the loved ones
+        in managing the estate in the event of death. Click HERE "link to
+        Sampul’s FAQ" for the process flow.
+      </p>
+      <p>"ACCEPT (sign up page)" "REJECT (page on next slide)"</p>
+      <p>
+        We hope you're excited to join the mission of safekeeping asset for the
+        loved ones.
+      </p>
+      <p>
+        There are currently RM 100 billion worth of frozen asset and unclaimed
+        monies in our country. Our mission is to ensure no one and no asset is
+        left behind in the estate planning process.
+      </p>
+      <p>The Sampul team</p>
     </div>
   );
 };
