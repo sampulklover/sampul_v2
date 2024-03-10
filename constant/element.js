db685c0502f12d88db7590dfa8b7aa85d29ed077
iklankerjakosongmalaysia <134831327+devOneGithub@users.noreply.github.com>
2024-03-11 03:24:36 +0800

    v1.0.60

--- constant/element.js
+++ constant/element.js
@@ -1,6 +1,6 @@
 export const addUserImg = `/images/profile_picture_of_man_in_green_shirt_and_orange_hat.png`;
 export const addFileImg = '/images/drag_drop_file.png';
-export const emptyUserImg = `https://image.pngaaa.com/291/5335291-middle.png`;
+export const emptyUserImg = `/images/profile_picture_of_man_in_green_shirt_and_orange_hat.png`;
 export const bucketName = 'images';
 export const pages = [
   {
