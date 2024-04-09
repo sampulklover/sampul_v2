5a40df8a5e3cb96ea6da5f9d13f0e321f8821da4
iklankerjakosongmalaysia <134831327+devOneGithub@users.noreply.github.com>
2024-04-10 07:40:38 +0800

    v1.1.34

--- pages/api/resend/email-status.js
+++ pages/api/resend/email-status.js
@@ -18,7 +18,7 @@ export default async function handler(req, res) {
       .update({
         email_status: payload.type,
       })
-      .eq('email_resend_id', '46d54d9b-a4db-4df2-b2db-a801dd0196dc');
+      .eq('email_resend_id', payload.data.email_id);
 
     if (error) {
       return res.status(400).json(error);
