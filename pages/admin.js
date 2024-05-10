56f51d8d4bd5989fe4956a7c0cd3f20f6b5ac97c
iklankerjakosongmalaysia <134831327+devOneGithub@users.noreply.github.com>
2024-05-11 04:41:34 +0800

    v1.1.62

--- pages/admin.js
+++ pages/admin.js
@@ -1,19 +1,19 @@
-import { useEffect, useState } from 'react';
-import { supabase } from '../utils/supabase';
-import { useRouter } from 'next/router';
-import toast from 'react-hot-toast';
-import Link from 'next/link';
-import Footer from '../components/Footer';
-import Breadcrumb from '../components/Breadcrumb';
-import SideBar from '../components/SideBar';
+import AccountsChart from '../components/AccountsChart';
 import AllUser from '../components/AllUser';
+import Breadcrumb from '../components/Breadcrumb';
+import Footer from '../components/Footer';
 import ManageBodies from '../components/ManageBodies';
+import SideBar from '../components/SideBar';
+import UsersChart from '../components/UsersChart';
 import { useApi } from '../context/api';
+import adminStyles from '../styles/admin.module.scss';
+import { supabase } from '../utils/supabase';
 import * as Sentry from '@sentry/nextjs';
 import { Inter } from 'next/font/google';
-import adminStyles from '../styles/admin.module.scss';
-import UsersChart from '../components/UsersChart';
-import AccountsChart from '../components/AccountsChart';
+import Link from 'next/link';
+import { useRouter } from 'next/router';
+import { useEffect, useState } from 'react';
+import toast from 'react-hot-toast';
 
 const inter = Inter({ subsets: ['latin'] });
 
@@ -108,169 +108,10 @@ const Admin = () => {
     );
   };
 
-  const chartMenuCard = ({
-    title = 'Title',
-    value = '0',
-    percentage = '0%',
-  }) => {
-    const status = 'danger';
-
-    const themeConfig = {
-      success: {
-        theme: adminStyles.success,
-        icon: (
-          <svg
-            width="12"
-            height="12"
-            viewBox="0 0 12 12"
-            fill="none"
-            xmlns="http://www.w3.org/2000/svg"
-          >
-            <g id="trend-up-01">
-              <path
-                id="Icon"
-                d="M11 3.5L7.06568 7.43432C6.86768 7.63232 6.76867 7.73133 6.65451 7.76842C6.55409 7.80105 6.44591 7.80105 6.34549 7.76842C6.23133 7.73133 6.13232 7.63232 5.93431 7.43431L4.56568 6.06568C4.36768 5.86768 4.26867 5.76867 4.15451 5.73158C4.05409 5.69895 3.94591 5.69895 3.84549 5.73158C3.73133 5.76867 3.63232 5.86768 3.43431 6.06569L1 8.5M11 3.5H7.5M11 3.5V7"
-                stroke="#17B26A"
-                stroke-width="1.5"
-                stroke-linecap="round"
-                stroke-linejoin="round"
-              />
-            </g>
-          </svg>
-        ),
-      },
-      danger: {
-        theme: adminStyles.danger,
-        icon: (
-          <svg
-            width="13"
-            height="12"
-            viewBox="0 0 12 12"
-            fill="none"
-            xmlns="http://www.w3.org/2000/svg"
-          >
-            <g id="trend-down-01" clip-path="url(#clip0_454_3681)">
-              <path
-                id="Icon"
-                d="M11.3333 8.5L7.39894 4.56568C7.20093 4.36768 7.10192 4.26867 6.98776 4.23158C6.88734 4.19895 6.77917 4.19895 6.67874 4.23158C6.56458 4.26867 6.46558 4.36768 6.26757 4.56569L4.89894 5.93432C4.70093 6.13232 4.60192 6.23133 4.48776 6.26842C4.38734 6.30105 4.27917 6.30105 4.17874 6.26842C4.06458 6.23133 3.96557 6.13232 3.76757 5.93431L1.33325 3.5M11.3333 8.5H7.83325M11.3333 8.5V5"
-                stroke="#F04438"
-                stroke-width="1.5"
-                stroke-linecap="round"
-                stroke-linejoin="round"
-              />
-            </g>
-            <defs>
-              <clipPath id="clip0_454_3681">
-                <rect
-                  width="12"
-                  height="12"
-                  fill="white"
-                  transform="translate(0.333252)"
-                />
-              </clipPath>
-            </defs>
-          </svg>
-        ),
-      },
-    };
-
-    return (
-      <>
-        <div className={adminStyles.tabContainer}>
-          <div className={adminStyles.tabHeader}>
-            <div className={adminStyles.tabTitle}>{title}</div>
-            <div className={adminStyles.totalUsers}>
-              <div className={adminStyles.totalUsersValue}>{value}</div>
-              <div
-                className={`${adminStyles.percentageContainer} ${themeConfig[status].theme}`}
-              >
-                {themeConfig[status].icon}
-                <div
-                  className={`${adminStyles.percentageValue} ${themeConfig[status].theme}`}
-                >
-                  {percentage}
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
-      </>
-    );
-  };
-
   const chartSection1 = () => {
     return (
-      <div class="project-tab">
-        <nav>
-          <div class="nav nav-tabs" id="nav-tab" role="tablist">
-            <button
-              class="nav-link active"
-              id="nav-users-tab"
-              data-bs-toggle="tab"
-              data-bs-target="#nav-users"
-              type="button"
-              role="tab"
-              aria-controls="nav-users"
-              aria-selected="true"
-            >
-              {chartMenuCard({
-                title: 'Total Users',
-                value: '0',
-                percentage: '0%',
-              })}
-            </button>
-            <button
-              class="nav-link"
-              id="nav-bodies-tab"
-              data-bs-toggle="tab"
-              data-bs-target="#nav-bodies"
-              type="button"
-              role="tab"
-              aria-controls="nav-bodies"
-              aria-selected="false"
-            >
-              {chartMenuCard({
-                title: 'Basic Users',
-                value: '0',
-                percentage: '0%',
-              })}
-            </button>
-            <button
-              class="nav-link"
-              id="nav-bodies-tab"
-              data-bs-toggle="tab"
-              data-bs-target="#nav-bodies"
-              type="button"
-              role="tab"
-              aria-controls="nav-bodies"
-              aria-selected="false"
-            >
-              {chartMenuCard({
-                title: 'Premium Users',
-                value: '0',
-                percentage: '0%',
-              })}
-            </button>
-          </div>
-        </nav>
-        <div class="tab-content" id="nav-tabContent">
-          <div
-            class="tab-pane fade show active"
-            id="nav-users"
-            role="tabpanel"
-            aria-labelledby="nav-users-tab"
-          >
-            <UsersChart summary={summary} refreshFunction={fetchData} />
-          </div>
-          <div
-            class="tab-pane fade"
-            id="nav-bodies"
-            role="tabpanel"
-            aria-labelledby="nav-bodies-tab"
-          >
-            {/* <ManageBodies summary={summary} refreshFunction={fetchData} /> */}
-          </div>
-        </div>
+      <div>
+        <UsersChart summary={summary} refreshFunction={fetchData} />
       </div>
     );
   };
@@ -382,11 +223,11 @@ const Admin = () => {
             <>
               <Breadcrumb pageName={'Admin'} />
               <div class="mt-4">{title()}</div>
-              {/* <div class="row mt-3">
-                <div class="col-md-7">{chartSection1()}</div>
-                <div class="col-md">{chartSection2()}</div>
-              </div> */}
-              <div class="row mt-3">{tabSection()}</div>
+              <div class="row mt-3">
+                <div class="col-md-7 mb-3">{chartSection1()}</div>
+                <div class="col-md-5">{chartSection2()}</div>
+              </div>
+              <div class="row mt-4">{tabSection()}</div>
             </>
           ) : (
             noPermissionView()
