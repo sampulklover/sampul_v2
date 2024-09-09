6f72166a17e8f62143af1761d2e5ff7332521004
Danish Imran <IamDanishImran@users.noreply.github.com>
2024-09-09 11:14:28 +0800

    Modified by danish

--- static/js/global-config.js
+++ static/js/global-config.js
@@ -1,18 +1,48 @@
-const formConfigs = [
-  {
-    containerId: 'footer-container',
-    formFunction: footer,
-  },
-  {
-    containerId: 'footer_BM',
-    formFunction: BM_footer,
-  }
-];
+// const formConfigs = [
+//   {
+//     lang:"bm",
+//     containerId: 'footer-container',
+//     formFunction: BI_footer,
+//   },
+//   {
+//     lang:'en',
+//     containerId: 'footer_BM',
+//     formFunction: BM_footer,
+//   }
+// ];
 
-formConfigs.forEach((item) => {
+
+const hello = () => {
+    const lang = localStorage.getItem('lang')
+    const bm = [{
+      containerId: 'footer-container',
+      formFunction: BM_footer,
+    }]
+
+    const en = [{
+      containerId: 'footer_BM',
+      formFunction: BM_footer
+      }]
+    if (lang === 'bm') {
+      console.log('in bm',bm)
+      return bm
+    } else {
+      console.log('in bi',en)
+      return en
+    }
+}
+
+const haha = hello()
+console.log('hahah' , haha)
+
+haha.forEach(async (item) => {
+
   const element = document.getElementById(item.containerId);
+
+  const isBM = element === "BI_footer"
+
   if (element) {
-    element.innerHTML = item.formFunction(); // Call the function here
+    element.innerHTML = item.formFunction();
   } else {
     console.error(`Element with ID ${item.containerId} not found.`);
   }
